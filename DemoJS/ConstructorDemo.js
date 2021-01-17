918233ff4ef2049a728b26c8fca1ffbd3e128975
Pooja <pansarepoojab@gmail.com>
2021-01-17 05:30:54 +0530

    Javascript Demos

---
+++ DemoJS/ConstructorDemo.js
@@ -0,0 +1,15 @@
+//function constructor
+var a = (function Person()
+{
+    
+this.name='John';
+    this.age=25;
+    alert(this.name);
+})();
+
+//object creation
+//const person = new Person();
+//const person2 = new Person();
+//document.write(person.name);
+//document.write('\n');
+//document.write(person2.age);
