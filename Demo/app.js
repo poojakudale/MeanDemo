4055d7cba57332c7133b158a48f104d9e3fc9307
Pooja <pansarepoojab@gmail.com>
2021-01-24 11:03:34 +0530

    Babel Demo

---
+++ Demo/app.js
@@ -0,0 +1,9 @@
+let sum =(a,b)=>{
+	console.log(a+b);
+}
+let multiply=(a,b)=>{
+	console.log(a*b);
+}
+
+sum(5,6);
+multiply(10,2);
---
+++ Demo/public/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var sum = function sum(a, b) {
+	console.log(a + b);
+};
+var multiply = function multiply(a, b) {
+	console.log(a * b);
+};
+
+sum(5, 6);
+multiply(10, 2);
