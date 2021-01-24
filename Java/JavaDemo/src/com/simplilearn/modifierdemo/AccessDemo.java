d65a5d0c65bd40613c9dde2421b94bcdc59c11c3
Pooja <pansarepoojab@gmail.com>
2021-01-24 10:49:52 +0530

    Detail Steps of Webpack and Babel

---
+++ Java/JavaDemo/src/com/simplilearn/modifierdemo/AccessDemo.java
@@ -0,0 +1,15 @@
+package com.simplilearn.modifierdemo;
+
+public class AccessDemo 
+{
+	public static void main(String []asr)
+	{
+		int x = 'a';
+		System.out.println("x : "+x);
+		int z = 145;
+		byte b =(byte) z;
+		System.out.println("b : " +b);
+	}
+	
+
+}
